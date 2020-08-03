96e1e100894ae40762b6ed3fd419f2a59106187c
Adrian Prusik <xpctnc@gmail.com>
2020-08-03 17:47:05 +0200

    Create basic App view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>spacer</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+export default { /* eslint-disable */
+  name: 'Home' /* eslint-disable */
+}  /* eslint-disable */
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-};
 </script>
